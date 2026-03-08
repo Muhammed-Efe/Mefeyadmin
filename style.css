693d7b7ab99a07c35dc33e8ed310e3d5337e383c
Muhammed-Efe <muhammedefefalcon@gmail.com>
2026-03-08 09:03:24 +0300

    Add initial styles for body and navigation

---
+++ style.css
@@ -0,0 +1,20 @@
+body{
+background:#0f172a;
+color:white;
+font-family:Arial;
+}
+
+nav{
+background:#111827;
+padding:15px;
+}
+
+nav a{
+color:white;
+margin:10px;
+text-decoration:none;
+}
+
+nav a:hover{
+color:#38bdf8;
+}
